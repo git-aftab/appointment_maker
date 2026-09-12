bd01329f4f6345db1b4a3361be0f0c2f7f39b9c6
aftab <mdaftab0726@gmail.com>
2026-09-12 21:09:44 +0530

    setting up better auth

---
+++ lib/auth.ts
@@ -0,0 +1,11 @@
+import { betterAuth } from "better-auth";
+import { pool } from "./db";
+
+
+export const auth = betterAuth({
+  database: pool,
+
+  emailAndPassword: {
+    enabled: true,
+  },
+});
